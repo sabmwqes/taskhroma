298d2c89ea2edccd9ff1b1b2c427dda216860624
sabmwqes <91858685+sabmwqes@users.noreply.github.com>
2025-09-08 23:18:39 +0900

    fix: NotFoundコンポーネントのマークアップを修正し、ページが見つからない場合のメッセージを追加

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -2,10 +2,13 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const NotFound: React.FC = () => (
-	<>
+	<div style={{ textAlign: "center", marginTop: "20%" }}>
 		<h1>404</h1>
+		<h3>Page Not Found</h3>
+		<br/>
+
 		<Link to="/home">Go to Home</Link>
-	</>
+	</div>
 );
 
 export default NotFound;
